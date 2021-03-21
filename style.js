e83f6a097bad445a7292981348c6774f44706c43
reymar bongolto <ramyer88@gmail.com>
2021-03-22 01:55:36 +0800

    css changes

--- style.js
+++ style.js
@@ -9,6 +9,24 @@ function myFunction() {
 
 
 
+// SMOOTH SCROLL  USING JQUERY//
+
+$('.topnav a').on('click', function(e){
+  if(this.hash !== ''){
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      1200);
+  }
+  
+});
+//******************************************//
+
 //IMAGE SLIDER //
 
   var slideIndex = 1;
