c39798357c50590d2c0e0cd900e8bd1bdf9fdd0a
Reymar Bongolto <ramyer88@gmail.com>
2021-11-29 20:21:15 +0800

    update portfolio

--- styles/style.js
+++ styles/style.js
@@ -7,7 +7,7 @@ jQuery(function(){
 // Nav bar toggle
 function navbar()
 {
-    $('.icon').on('click', function(event){
+    $('.icon').on('click', function(){
         var x = document.getElementById("my-topnav");
         if (x.className === "top-nav")
         {
@@ -33,54 +33,4 @@ function smoothScroll()
             1200);
         }
     });
-}
-
-// Sending Email
-function sendEmail()
-{
-    $(document).on('click', '#submit', function(event){
-        event.preventDefault();
-        let name = $('#name').val();
-        let email = $('#email').val();
-        let message = $('#message').val();
-        let regx_name = /^([a-z\s])+$/i;
-        let regx_email = /^([a-z0-9_\-\.])+\@([a-z0-9_\-\.])+\.([a-z]{2,4})$/i;
-        let regx_space = /^([\s])+$/;
-        $('.frm-inputs').removeClass('err');
-        if(!regx_name.test(name) || name==="" || regx_space.test(name) || name.length > 100)
-        {
-            $('#name').addClass('err');
-            return false;
-        }
-        if(!regx_email.test(email) || email.length > 100)
-        {
-            $('#email').addClass('err');
-            return false;
-        }
-        if( message==="" || regx_space.test(message) || message.length > 300)
-        {
-            $('#message').addClass('err');
-            return false;
-        }
-        $.ajax({
-            method:'POST',
-            url:'https://formsubmit.co/ajax/ramyer88@gmail.com',
-            dataType:'json',
-            accepts:'application/json',
-            data:{
-                name:name,
-                email:email,
-                message:message
-            },
-            success:function(){
-                alert('message sent');
-                $('form').trigger('reset');
-            },
-        })
-        return false;
-    })
-    $(document).on('click', '#clear', function(){
-        $('.frm-inputs').removeClass('err');
-        $('form').trigger('reset');
-    })
 }
